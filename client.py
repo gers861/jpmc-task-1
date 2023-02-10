500e1095bc39d5763c0ec52382106d2147184071
gers861 <gers861@gmail.com>
2023-02-10 13:16:25 +0000

    Update client.py

--- client.py
+++ client.py
@@ -41,7 +41,6 @@ def getDataPoint(quote):
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
     if price_b==0:
         return
 
